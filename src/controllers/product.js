7802b3d32eb756652c133be3f96601d6ed7d1aff
Ady <polcsek.dm90@gmail.com>
2021-11-09 16:36:56 -0800

    chore: add pagination to findmany

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -4,7 +4,25 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient({ rejectOnNotFound: true });
 
 const getAll = asyncHandler(async (req, res, next) => {
-    const products = await prisma.product.findMany();
+    /*
+        get page query string from url
+        parse string to integer && create skip number for pagination
+        check if skip value is a pozitive value
+    */
+    let { page } = req.query;
+
+    page = parseInt(page, 10);
+    toSkip = (page - 1 || 0) * 3;
+
+    if (toSkip < 0) toSkip = 0;
+
+    /*
+        return list of products based on pagination
+    */
+    const products = await prisma.product.findMany({
+        skip: toSkip,
+        take: 3,
+    });
 
     res.status(200).json({
         message: 'Successfully fetched',
