e838e9fa0223e645bc97ea879fbede004013a7ae
Ady <polcsek.dm90@gmail.com>
2021-11-13 22:48:47 -0800

    chore: create service funcs

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,16 +1,84 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+const { NotFound } = require('../errors');
 
-const getAll = async (options) => {};
+const Product = new PrismaClient().product;
 
-const getOne = async (productId) => {};
+const getAll = async (options) => {
+    let { page } = options;
 
-const createOne = async () => {};
+    page &&= parseInt(page, 10);
 
-const updateOne = async (productId) => {};
+    page ||= 1;
 
-const deleteOne = async (productId) => {};
+    const skip = (page - 1) * 3;
+    const take = 4;
+
+    const products = await Product.findMany({
+        skip,
+        take,
+        orderBy: {
+            createdAt: 'desc',
+        },
+    });
+
+    return products;
+};
+
+const getOne = async (productId) => {
+    const product = await Product.findFirst({
+        where: {
+            id: productId,
+        },
+    });
+
+    if (!product) {
+        throw new NotFound(`Product with ${productId} was not found`);
+    }
+
+    return product;
+};
+
+const createOne = async ({ name, description, price }) => {
+    const product = await Product.create({
+        data: {
+            name,
+            description: description || '',
+            price,
+        },
+    });
+
+    return product;
+};
+
+const updateOne = async (productId, data) => {
+    const product = await Product.update({
+        where: {
+            id: productId,
+        },
+        data,
+    });
+
+    if (!product) {
+        throw new NotFound(`Product with ${productId} was not found`);
+    }
+
+    return product;
+};
+
+const deleteOne = async (productId) => {
+    const product = await Product.delete({
+        where: {
+            id: productId,
+        },
+    });
+
+    if (!product) {
+        throw new NotFound(`Product with ${productId} was not found`);
+    }
+
+    return product;
+};
 
 const productService = {
     getAll,
