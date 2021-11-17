9a8221e17c6ae03d704c6de802e64329fa6fc09b
Ady <polcsek.dm90@gmail.com>
2021-11-17 15:06:20 -0800

    chore: remove UNEC logic

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,51 +1,15 @@
-const { Prisma } = require('@prisma/client');
-const { CustomError } = require('../errors');
 const logger = require('../utils/logger');
-
-const errors = {
-    P2002: {
-        code: 409,
-        message: '',
-    },
-    P2025: {
-        code: 404,
-        message: 'Product was not found',
-    },
-};
+const errorParser = require('../utils/errorParser');
 
 const errorHandler = (err, req, res, next) => {
-    let error = { ...err };
-    error.message = err.message;
+    const { statusCode, message } = errorParser(err);
 
     if (process.env.NODE_ENV === 'dev') {
         logger.error(err.stack);
     }
 
-    if (err instanceof CustomError) {
-        error.code = err.code;
-        error.message = err.message;
-    }
-
-    // Prisma query errors
-    if (err instanceof Prisma.PrismaClientKnownRequestError) {
-        error = errors[err.code];
-
-        if (!error.message) {
-            error.message = `Target with '${err.meta.target}' already exists`;
-        }
-    }
-
-    // Invalid Id || Missing value from body
-    if (err instanceof Prisma.PrismaClientValidationError) {
-        const msg = err.message.split('Argument')[1];
-        error.message = msg.includes('username')
-            ? 'Please enter a username'
-            : 'Please enter an email';
-        error.code = 400;
-    }
-
-    res.status(error.code || 500).json({
-        error: error.message || 'Server Error',
+    res.status(statusCode || 500).json({
+        error: message || 'Server Error',
     });
 };
 
