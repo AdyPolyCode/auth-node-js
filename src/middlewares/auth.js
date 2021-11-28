a770cc95c42d8fc46d7be4e06edcfc6031060521
Ady <polcsek.dm90@gmail.com>
2021-11-27 17:29:49 -0800

    chore: fix token from header + send user

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,23 +1,16 @@
 const userService = require('../services/user.service');
-const tokenService = require('../services/token.service');
-const { UnAuthorized } = require('../errors');
 
 const authenticate = async (req, res, next) => {
-    const headerToken = req.headers.authorization;
-    if (!headerToken || !headerToken.startsWith('Bearer ')) {
-        return next(new UnAuthorized('Invalid token'));
+    const tokenString = req.headers['x-authorization'];
+    try {
+        const user = await userService.getByTokenString(tokenString);
+
+        req.user = user;
+
+        next();
+    } catch (error) {
+        next(error);
     }
-
-    const tokenString = req.headers.authorization.split(' ');
-
-    const token = await tokenService.getByTokenString(tokenString[1]);
-    if (!token.active) {
-        return next(new UnAuthorized('Invalid token, token is inactive'));
-    }
-
-    await userService.getByTokenString(tokenString[1]);
-
-    next();
 };
 
 module.exports = authenticate;
