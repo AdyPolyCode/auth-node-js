f2c508166af6ffba2f9e91bf974a6789719e98ba
Ady <polcsek.dm90@gmail.com>
2021-12-01 07:38:04 -0800

    chore: create mailing + password service for users

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,8 +1,57 @@
 const encryptionService = require('./encryption.service');
 const userService = require('./user.service');
 const tokenService = require('./token.service');
+const mailService = require('./mail.service');
 
-const register = async (username, email, password) => {
+const sendConfirmationMail = async (email, id, options) => {
+    const { tokenString } = await tokenService.createToken(id);
+
+    const generatedUrl = mailService.generateUrl(
+        options,
+        'mail-confirmation',
+        tokenString
+    );
+
+    await mailService.sendEmail(email, 'mail-confirmation', generatedUrl);
+};
+
+const sendPasswordResetMail = async (email, options) => {
+    const user = await userService.getByEmail(email);
+
+    const { tokenString } = await tokenService.createToken(user.id);
+
+    const generatedUrl = mailService.generateUrl(
+        options,
+        'mail-confirmation',
+        tokenString
+    );
+
+    await mailService.sendEmail(email, 'password-reset', generatedUrl);
+};
+
+const changePassword = async (tokenString, password1, password2) => {
+    const user = await userService.getByTokenString(tokenString);
+
+    const hashedPassword = encryptionService.createHash(user.salt, password1);
+
+    await userService.updateOne(user.id, {
+        hashedPassword,
+    });
+
+    await tokenService.deactivateToken(tokenString);
+
+    return user;
+};
+
+const confirmMail = async (tokenString) => {
+    const user = await userService.getByTokenString(tokenString);
+
+    await userService.updateOne(user.id, { isVerified: true });
+
+    await tokenService.deactivateToken(tokenString);
+};
+
+const register = async (username, email, password, options) => {
     const salt = encryptionService.createSalt();
 
     const hash = encryptionService.createHash(salt, password);
@@ -10,6 +59,8 @@ const register = async (username, email, password) => {
     const user = await userService.createOne(username, email, hash, salt);
 
     const token = await tokenService.createToken(user.id);
+
+    await sendConfirmationMail(user.email, user.id, options);
 
     return { user, token };
 };
@@ -29,14 +80,6 @@ const logout = async (tokenString) => {
 
     await tokenService.deactivateToken(token.tokenString);
 };
-
-const sendConfirmationMail = async (email) => {};
-
-const sendPasswordResetMail = async (email) => {};
-
-const changePassword = async (tokenString) => {};
-
-const confirmMail = async () => {};
 
 const authService = {
     register,
