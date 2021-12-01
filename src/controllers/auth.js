63520cbe3dc9682f701e0615c197ba95d3e53d23
Ady <polcsek.dm90@gmail.com>
2021-12-01 07:32:20 -0800

    chore: add new validators + endpoints

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,9 +4,7 @@ const authService = require('../services/auth.service');
 const login = asyncHandler(async (req, res, next) => {
     const { email, password } = req.body;
 
-    const validLogin = await authService.login(email, password);
-
-    const { user, token } = validLogin;
+    const { user, token } = await authService.login(email, password);
 
     res.status(200).json({
         message: `Successfully logged in as ${user.username}`,
@@ -23,14 +21,20 @@ const login = asyncHandler(async (req, res, next) => {
 const register = asyncHandler(async (req, res, next) => {
     const { username, email, password } = req.body;
 
+    const options = {
+        host: req.get('host'),
+        path: '/api/auth/mail-confirmation',
+    };
+
     const { user, token } = await authService.register(
         username,
         email,
-        password
+        password,
+        options
     );
 
     res.status(201).json({
-        message: `${user.username} successfully created`,
+        message: `${user.username} successfully created & confirmation email sent`,
         data: {
             token: token.tokenString,
         },
@@ -48,8 +52,65 @@ const logout = asyncHandler(async (req, res, next) => {
     });
 });
 
+const forgotPassword = asyncHandler(async (req, res, next) => {
+    const { email } = req.body;
+
+    const options = {
+        host: req.get('host'),
+        path: '/api/auth/password-reset',
+    };
+
+    await authService.sendPasswordResetMail(email, options);
+
+    res.status(200).json({ message: 'Reset password email successfully sent' });
+});
+
+const resetPassword = asyncHandler(async (req, res, next) => {
+    const { resetToken } = req.params;
+    const { password1, password2 } = req.body;
+
+    const user = await authService.changePassword(
+        resetToken,
+        password1,
+        password2
+    );
+
+    res.status(200).json({ message: 'Password successfully changed', user });
+});
+
+const confirmMail = asyncHandler(async (req, res, next) => {
+    const { mailToken } = req.params;
+
+    await authService.confirmMail(mailToken);
+
+    res.status(200).json({ message: 'Email successfully confirmed' });
+});
+
+function renderConfirmMail(req, res) {
+    const url = `http://${req.get('host')}/api/auth${req.path}`;
+
+    res.render('mail-confirmation', {
+        title: 'Mail-Confirmation',
+        url,
+    });
+}
+
+function renderResetPassword(req, res) {
+    const url = `http://${req.get('host')}/api/auth${req.path}`;
+
+    res.render('password-reset', {
+        title: 'Password-Reset',
+        url,
+    });
+}
+
 module.exports = {
     login,
     register,
     logout,
+    forgotPassword,
+    resetPassword,
+    renderResetPassword,
+    confirmMail,
+    renderConfirmMail,
 };
