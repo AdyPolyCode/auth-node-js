1d9fbcaa3b13f7adb36afc07610d432bb3336b0a
Ady <polcsek.dm90@gmail.com>
2021-12-01 07:33:03 -0800

    chore: add mail confirmation check

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,14 @@
 const userService = require('../services/user.service');
+const { BadRequest } = require('../errors');
 
 const authenticate = async (req, res, next) => {
     try {
         const tokenString = req.headers['x-authorization'];
         const user = await userService.getByTokenString(tokenString);
+
+        if (!user.isVerified) {
+            next(new BadRequest('Please confirm your email'));
+        }
 
         req.user = user;
 
