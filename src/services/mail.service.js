2fafc84f4d62fd803c560a93fb5cf7e8facd5777
Ady <polcsek.dm90@gmail.com>
2021-12-08 12:22:33 -0800

    chore: add pkgs + fix template sending

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -1,24 +1,37 @@
 const nodemailer = require('nodemailer');
+const hbs = require('handlebars');
+const { dirname, join } = require('path');
+const { readFileSync } = require('fs');
 const { CustomError } = require('../errors');
+
+const root = dirname(__dirname);
 
 const options = {
     'mail-confirmation': {
         host: `${process.env.NODE_HOST}:${process.env.NODE_PORT}`,
-        path: '/api/auth/mail-confirmation',
+        urlPath: '/api/auth/mail-confirmation',
+        templatePath: join(root, 'templates/mail-confirmation.hbs'),
         message: 'Please confirm this email so you can enjoy our services - ',
     },
     'password-reset': {
         host: `${process.env.NODE_HOST}:${process.env.NODE_PORT}`,
-        path: '/api/auth/password-reset',
+        urlPath: '/api/auth/password-reset',
+        templatePath: join(root, 'templates/password-reset.hbs'),
         message: 'Here is your reset password email - ',
     },
 };
 
 const sendEmail = async (email, type, tokenString) => {
     try {
-        const { host, path } = options[type];
+        const { host, urlPath, templatePath } = options[type];
 
-        const url = `http://${host}${path}/${tokenString}`;
+        const url = `http://${host}${urlPath}/${tokenString}`;
+
+        const templateFile = readFileSync(templatePath, {
+            encoding: 'utf-8',
+        });
+
+        const template = hbs.compile(templateFile)({ URL: url });
 
         const message = options[type].message.concat(url);
 
@@ -37,11 +50,12 @@ const sendEmail = async (email, type, tokenString) => {
             to: email,
             subject: type,
             text: message,
+            html: template,
         });
 
         return url;
     } catch (error) {
-        throw new CustomError('Could not send the email', 500);
+        throw new CustomError('Cannot send email', 500);
     }
 };
 
