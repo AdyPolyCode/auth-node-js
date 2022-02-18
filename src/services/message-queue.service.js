5fb2e10046600691a2e54386c30e42d2fc52b2f9
Ady <polcsek.dm90@gmail.com>
2022-02-18 12:11:23 +0100

    chore: fix ex/queue bindings

--- src/services/message-queue.service.js
+++ src/services/message-queue.service.js
@@ -9,15 +9,16 @@ const publish = async (data, options) => {
 
         const channel = await connection.createChannel();
 
-        const exchange = await channel.assertExchange('task_e', 'direct', {
+        const exchange = await channel.assertExchange('exOne', 'direct', {
             durable: true,
         });
 
-        channel.publish(
+        await channel.publish(
             exchange.exchange,
             options.severity,
             Buffer.from(message),
             {
+                persistent: true,
                 contentType: 'application/json',
             }
         );
@@ -26,41 +27,37 @@ const publish = async (data, options) => {
     }
 };
 
-const subscribe = async (data, queueName) => {
+const subscribe = async (data, options) => {
     try {
         const connection = await amqp.connect(process.env.RABBIT_URL);
 
         const channel = await connection.createChannel();
 
-        const exchange = await channel.assertExchange('task_e', 'direct', {
+        const exchange = await channel.assertExchange('exOne', 'direct', {
             durable: true,
         });
 
-        const queue = await channel.assertQueue(queueName, {
+        const queue = await channel.assertQueue(options.queueName, {
             durable: true,
         });
 
-        await channel.bindQueue(queue.queue, exchange.exchange, 'direct');
-
-        await channel.consume(
+        await channel.bindQueue(
             queue.queue,
-            async (msg) => {
-                const options = JSON.parse(msg.content.toString());
-
-                try {
-                    await data.sendMail(options);
-
-                    channel.ack(msg);
-                } catch (error) {
-                    channel.nack(msg);
-
-                    throw error;
-                }
-            },
-            {
-                noAck: false,
-            }
+            exchange.exchange,
+            options.severity
         );
+
+        await channel.consume(queue.queue, async (msg) => {
+            const message = JSON.parse(msg.content.toString('utf8'));
+
+            try {
+                await data.sendMail(message);
+
+                channel.ack(msg);
+            } catch (error) {
+                channel.nack(msg);
+            }
+        });
     } catch (error) {
         throw error;
     }
