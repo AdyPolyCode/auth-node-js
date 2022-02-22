d18187266242199ab5ba413941f691452ed014b6
Ady <polcsek.dm90@gmail.com>
2022-02-22 20:52:00 +0100

    chore: add additional export from auth

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('./async-handler');
-const auth = require('./auth');
+const { authenticate, checkVerification } = require('./auth');
 const errorHandler = require('./error-handler');
 const notFoundHandler = require('./not-found-handler');
 const validateBody = require('./validate-body');
 
 module.exports = {
     asyncHandler,
-    auth,
+    authenticate,
+    checkVerification,
     errorHandler,
     notFoundHandler,
     validateBody,
